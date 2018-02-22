cc8547bf6970e14ffccf853f27d70ca16319ac59
ramon-egger <ramonegger@hotmail.com>
2018-02-22 17:09:34 +0100

    master

--- create_encrpyted_backup/backup.sh
+++ create_encrpyted_backup/backup.sh
@@ -8,20 +8,65 @@ username0=/path/to/username_file #I recommend not to hardcode your credentials i
 
 
 
-
 		#Database Info
 
-bdir=/path/to/backup_directory 		#directory for your backup
+bdir=/root/test/	 		#directory for your backup
 dbase0="Database_name" 			#add more databases if you want
-tempdir=/path/to/temporary_directory	#temporary data is been saved and deleted here. Do use a / at the end!
+tempdir=/root/temp			#temporary data is been saved and deleted here. Do use a / at the end!
 dt=`date +"%m-%d-%y"`			#define date for filename and structure
 filename="Filename_here"		#define a filename which is infront of the $dt
 filetype="sql"				#define filetype. Default = .sql
-
-dumpname="$filename"."$filetype"
+dump0="$tempdir""$filename"."$filetype"
+error="0"
+status="0"
 		#Lets begin
 
-#mkdir -p $bdir
-printf "Starting backup..\n"
-echo "$dumpname"
-#mysqldump -u $username0 -p --databases $dbase0 --password=$password0 > $tempdir
+while [$status="0"] do
+
+while [$error="0"] do
+
+if [ -d "$bdir"  ]; then
+
+		if [ -d "$tempdir"  ]; then
+
+	printf "Starting backup.."
+	#mysqldump -u $username0 -p --databases $dbase0 --password=$password0 > $dump0
+
+		else
+
+			echo "$tempdir doesn't exist"
+
+				read -p "Do you want to create it? (y/n)" choice0
+				case "$choice0" in
+					y|Y ) mkdir $tempdir && error="1";;
+					n|N ) echo "Bye" && status="1";;
+					* ) echo "invalid";;
+				esac
+		fi
+
+		else
+
+			echo "$bdir doesn't exist"
+
+                                read -p "Do you want to create it? (y/n)" choice1
+                                case "$choice1" in
+                                        y|Y ) mkdir $bdir && error="1";;
+                                        n|N ) echo "Bye" && status="1";;
+                                        * ) echo "invalid";;
+				esac
+
+		fi
+
+#done
+
+				echo "hmmm..."
+
+                                read -p "Re-run script? (y/n)" choice2
+                                case "$choice2" in
+                                        y|Y ) error="0";;
+                                        n|N ) status="1";;
+                                        * ) echo "invalid";;
+                                esac
+
+
+done
